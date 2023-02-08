b579a0a6fa1f779807b8b5a84bfbab00d9dc8544
Aaron Gregg <emsixteen@gmail.com>
2023-02-08 12:55:57 +0100

    Added: Show full description on Finn

---
+++ finn/finn-showEntireDescription.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Show entire description automatically
+// @namespace    finndotno
+// @version      1.0
+// @description  Skip the annoying extra step of having to click the "Vis hele beskrivelsen" button
+// @author       Aaron Gregg
+// @match        *://*.finn.no/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=finn.no
+// @grant        GM_addStyle
+// @run-at       document-end
+// ==/UserScript==
+
+const showEntireDescriptionButton =
+  "button[data-testid='show-entire-description']";
+
+(function () {
+  "use strict";
+  document.querySelector(showEntireDescriptionButton).click();
+})();
