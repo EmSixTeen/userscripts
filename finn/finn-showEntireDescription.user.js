f2d4071a393db890718cb5947cadd2539a1f46ca
Aaron Gregg <emsixteen@gmail.com>
2023-02-08 12:58:26 +0100

    Tweak: Changed to doc-idle

--- finn/finn-showEntireDescription.user.js
+++ finn/finn-showEntireDescription.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Show entire description automatically
 // @namespace    finndotno
-// @version      1.0
+// @version      1.1
 // @description  Skip the annoying extra step of having to click the "Vis hele beskrivelsen" button
 // @author       Aaron Gregg
 // @match        *://*.finn.no/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=finn.no
 // @grant        GM_addStyle
-// @run-at       document-end
+// @run-at       document-idle
 // ==/UserScript==
 
 const showEntireDescriptionButton =
