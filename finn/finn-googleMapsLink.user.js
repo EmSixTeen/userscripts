6b82d2a218c62d4156bcd252b1b9d2437f68d6a7
Aaron Gregg <emsixteen@gmail.com>
2023-07-27 11:24:47 +0200

    Added: Google maps link for Finn

---
+++ finn/finn-googleMapsLink.user.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         Google Maps link
+// @namespace    finndotno
+// @version      1.0
+// @description  Change the map link to Google Maps on Finn adverts and open it in a new tab
+// @author       Aaron Gregg
+// @match        *://*.finn.no/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=finn.no
+// @grant        GM_addStyle
+// @run-at       document-idle
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  // Function to check if the aria-label attribute starts with the given text
+  function startsWithAriaLabel(element, text) {
+    const ariaLabel = element.getAttribute("aria-label");
+    return ariaLabel && ariaLabel.startsWith(text);
+  }
+
+  // Function to add the style attribute to the element
+  function addStyleToElement(element) {
+    element.style.outline = "2px solid red";
+    element.style.display = "inline-block";
+  }
+
+  // Function to extract postcode and location from aria-label
+  function extractPostcodeAndLocation(element) {
+    const ariaLabel = element.getAttribute("aria-label");
+    const startIndex =
+      ariaLabel.indexOf("Åpne kart for") + "Åpne kart for".length;
+    const postcodeAndLocation = ariaLabel.substr(startIndex).trim();
+    return postcodeAndLocation;
+  }
+
+  // Function to construct the Google Maps link
+  function getGoogleMapsLink(postcodeAndLocation) {
+    const baseUrl = "https://www.google.com/maps/search/?api=1&query=";
+    const encodedAddress = encodeURIComponent(postcodeAndLocation);
+    return baseUrl + encodedAddress;
+  }
+
+  // Get all <a> elements on the page
+  const allLinks = document.getElementsByTagName("a");
+
+  // Loop through the links to find the one with the specified aria-label
+  for (const link of allLinks) {
+    if (startsWithAriaLabel(link, "Åpne kart for")) {
+      addStyleToElement(link);
+      const postcodeAndLocation = extractPostcodeAndLocation(link);
+      const googleMapsLink = getGoogleMapsLink(postcodeAndLocation);
+
+      // Update the link href to open in a new tab
+      link.setAttribute("href", googleMapsLink);
+      link.setAttribute("target", "_blank");
+
+      // Create a <span> element with the "(open in Google Maps)" text and add the "maps-label" class
+      const openInGoogleMapsSpan = document.createElement("span");
+      openInGoogleMapsSpan.textContent = " (open in Google Maps)";
+      openInGoogleMapsSpan.classList.add("maps-label");
+
+      // Create a <div> element and add the specified classes
+      const containerDiv = document.createElement("div");
+      containerDiv.classList.add(
+        "text-16",
+        "text-blue-600",
+        "flex",
+        "items-center",
+        "mt-32",
+        "mb-8"
+      );
+
+      // Append the <a> element and <span> element to the <div> element
+      containerDiv.appendChild(link);
+      containerDiv.appendChild(openInGoogleMapsSpan);
+
+      // Replace the <a> element with the <div> element in the DOM
+      link.parentNode.replaceChild(containerDiv, link);
+
+      break; // Stop after the first matching link
+    }
+  }
+})();
